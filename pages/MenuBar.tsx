596d6a2e2b84d35ee123ef4416c667a294dd4cc3
Madhur20 <59988556+Madhur20@users.noreply.github.com>
2022-08-01 19:37:35 -0500

    storeMenuIndex

--- pages/MenuBar.tsx
+++ pages/MenuBar.tsx
@@ -6,8 +6,17 @@ import Tab from '@mui/material/Tab';
 export default function CenteredTabs() {
   const [value, setValue] = React.useState(0);
 
+  React.useEffect(() => {
+    const menu_data = window.localStorage.getItem('MENU_INDEX');
+    if (menu_data !== null) setValue(JSON.parse(menu_data));
+    console.log('menu_value', menu_data);
+  }, [])
+
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
+    window.localStorage.setItem('MENU_INDEX', JSON.stringify(newValue));
+    console.log(newValue);
+    
   };
 
   return (
@@ -15,7 +24,7 @@ export default function CenteredTabs() {
       <Tabs value={value} onChange={handleChange} textColor="inherit" indicatorColor="secondary" centered>
         <Tab label="Home" />
         <Tab label="My Games" />
-        <Tab label="Friends" />
+        <Tab  label="Friends" />
         <Tab label="How To" />
       </Tabs>
     </Box>
