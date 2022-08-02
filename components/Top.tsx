456abb8e5163d511395c7785b79e6f1a0f5474f8
Madhur20 <59988556+Madhur20@users.noreply.github.com>
2022-08-02 18:47:36 -0500

    GamesPage

--- components/Top.tsx
+++ components/Top.tsx
@@ -5,12 +5,14 @@ import styles from '../styles/Home.module.css';
 
 export default function Top(props: any) {
     return (
-        <div className={styles.container1}>
-            <div className={styles.container2}>
-                <ButtonAppBar></ButtonAppBar>
-            </div>
-            <div className={styles.container2}>
-                <CenteredTabs page={props.page} />
+        <div>
+            <div className={styles.container1}>
+                <div className={styles.container2}>
+                    <ButtonAppBar></ButtonAppBar>
+                </div>
+                <div className={styles.container2}>
+                    <CenteredTabs page={props.page} />
+                </div>
             </div>
         </div>
     )
--- pages/games.tsx
+++ pages/games.tsx
@@ -2,12 +2,16 @@ import { Typography } from '@mui/material';
 import { NextPage } from 'next';
 import React from 'react';
 import Top from '../components/Top';
+import EnhancedTable from './GamesList';
+import styles from '../styles/Home.module.css';
 
 const Games: NextPage = () => {
     return (
         <div>
             <Top page={1} />
-            <Typography>Games</Typography>
+            <div>
+                <EnhancedTable></EnhancedTable> 
+            </div>
         </div>
     )
 }
