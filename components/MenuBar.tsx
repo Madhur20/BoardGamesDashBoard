90770d97dc099e3b12355c811bd1f9ee227918de
Anirudh Umarji <anirudh.umarji@gmail.com>
2022-08-01 20:05:30 -0500

    routing_02

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -2,21 +2,19 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function CenteredTabs() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+  const router = useRouter();
 
   return (
     <Box sx={{ width: '100%', bgcolor: '#333333' }}>
-      <Tabs value={value} onChange={handleChange} textColor="inherit" indicatorColor="secondary" centered>
-        <Tab label="Home" href={"/"} />
-        <Tab label="My Games" href={"/games"} />
-        <Tab label="Friends" href={"/friends"} />
-        <Tab label="How To" href={"/guide"} />
+      <Tabs textColor="inherit" indicatorColor="secondary" centered>
+        <Link href="/" prefetch={true}><Tab label="Home" /></Link>
+        <Link href="/games"><Tab label="Games" /></Link>
+        <Link href="/friends"><Tab label="Friends" /></Link>
+        <Link href="/guide"><Tab label="Guide" /></Link>
       </Tabs>
     </Box>
   );
