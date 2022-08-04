6f885b42d7e702a818ff90926205cfaa839262e0
Anirudh Umarji <anirudh.umarji@gmail.com>
2022-08-03 19:57:13 -0500

    bug fix

--- components/games/AddGameForm.tsx
+++ components/games/AddGameForm.tsx
@@ -3,11 +3,12 @@ import Grid from "@mui/material//Grid";
 import TextField from "@mui/material/TextField";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
-import Select from "@mui/material/Select";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import Slider from "@mui/material/Slider";
 import Button from "@mui/material/Button";
 import Snackbar from "@mui/material/Snackbar";
+import Box from "@mui/material/Box"
 import MuiAlert, { AlertProps } from '@mui/material/Alert';
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
@@ -29,6 +30,14 @@ export default function AddGameForm() {
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target as HTMLInputElement;
+        setFormValues({
+            ...formValues,
+            [name]: value,
+        });
+    };
+
+    const handleInputSelect = (event: SelectChangeEvent) => {
+        const { name, value } = event.target as HTMLInputElement;
         setFormValues({
             ...formValues,
             [name]: value,
@@ -70,90 +79,92 @@ export default function AddGameForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <Grid container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Grid sx={{ margin: '2%' }}>
-                    <TextField
-                        id="name-input"
-                        name="name"
-                        label="Name"
-                        type="text"
-                        value={formValues.name}
-                        onChange={handleInputChange}
-                    />
-                </Grid>
-                <Grid sx={{ margin: '2%' }}>
-                    <TextField
-                        id="max-players-input"
-                        name="maxPlayers"
-                        label="Max Players"
-                        type="number"
-                        value={formValues.maxPlayers}
-                        onChange={handleInputChange}
-                    />
-                </Grid>
-                <Grid sx={{ margin: '2%' }}>
-                    <FormControl>
-                        <FormLabel id="genre">Genre</FormLabel>
-                        <Select
-                            aria-labelledby="genre"
-                            name="genre"
-                            value={formValues.genre}
+        <Box>
+            <form onSubmit={handleSubmit}>
+                <Grid container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Grid sx={{ margin: '2%' }}>
+                        <TextField
+                            id="name-input"
+                            name="name"
+                            label="Name"
+                            type="text"
+                            value={formValues.name}
                             onChange={handleInputChange}
-                        >
-                            <MenuItem key="action" value="Action">
-                                Action
-                            </MenuItem>
-                            <MenuItem key="windows" value="Sports">
-                                Sports
-                            </MenuItem>
-                            <MenuItem key="fantasy" value="Fantasy">
-                                Fantasy
-                            </MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid sx={{ margin: '2%' }}>
-                    <div style={{ width: "350px" }}>
-                        Rating
-                        <Slider
-                            value={formValues.rating}
-                            onChange={handleSliderChange("rating")}
-                            defaultValue={0}
-                            step={1}
-                            min={0}
-                            max={100}
-                            marks={[
-                                {
-                                    value: 25,
-                                    label: "25",
-                                },
-                                {
-                                    value: 50,
-                                    label: "50",
-                                },
-                                {
-                                    value: 75,
-                                    label: "75",
-                                },
-                            ]}
-                            valueLabelDisplay="on"
                         />
-                    </div>
+                    </Grid>
+                    <Grid sx={{ margin: '2%' }}>
+                        <TextField
+                            id="max-players-input"
+                            name="maxPlayers"
+                            label="Max Players"
+                            type="number"
+                            value={formValues.maxPlayers}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid sx={{ margin: '2%' }}>
+                        <FormControl>
+                            <FormLabel id="genre">Genre</FormLabel>
+                            <Select
+                                aria-labelledby="genre"
+                                name="genre"
+                                value={formValues.genre}
+                                onChange={handleInputSelect}
+                            >
+                                <MenuItem key="action" value="Action">
+                                    Action
+                                </MenuItem>
+                                <MenuItem key="windows" value="Sports">
+                                    Sports
+                                </MenuItem>
+                                <MenuItem key="fantasy" value="Fantasy">
+                                    Fantasy
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid sx={{ margin: '2%' }}>
+                        <div style={{ width: "350px" }}>
+                            Rating
+                            <Slider
+                                value={formValues.rating}
+                                onChange={handleSliderChange("rating")}
+                                defaultValue={0}
+                                step={1}
+                                min={0}
+                                max={100}
+                                marks={[
+                                    {
+                                        value: 25,
+                                        label: "25",
+                                    },
+                                    {
+                                        value: 50,
+                                        label: "50",
+                                    },
+                                    {
+                                        value: 75,
+                                        label: "75",
+                                    },
+                                ]}
+                                valueLabelDisplay="on"
+                            />
+                        </div>
+                    </Grid>
+                    <Button variant="contained" color="primary" type="submit">
+                        Add Game
+                    </Button>
+                    <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={snackbarClose}>
+                        {success ?
+                            <Alert onClose={snackbarClose} severity="success" sx={{ width: '100%' }}>
+                                Game Inserted! Enjoy
+                            </Alert>
+                            : <Alert onClose={snackbarClose} severity="error" sx={{ width: '100%' }}>
+                                Invalid Input :( Please Try again.
+                            </Alert>}
+                    </Snackbar>
                 </Grid>
-                <Button variant="contained" color="primary" type="submit">
-                    Submit
-                </Button>
-                <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={snackbarClose}>
-                    {success ?
-                        <Alert onClose={snackbarClose} severity="success" sx={{ width: '100%' }}>
-                            Game Inserted! Enjoy
-                        </Alert>
-                        : <Alert onClose={snackbarClose} severity="error" sx={{ width: '100%' }}>
-                            Invalid Input :( Please Try again.
-                        </Alert>}
-                </Snackbar>
-            </Grid>
-        </form>
+            </form>
+        </Box>
     )
 }
