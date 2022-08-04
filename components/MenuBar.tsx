a86d0a40af49e8cbbd8ac39908c44979d02639bc
Madhur20 <59988556+Madhur20@users.noreply.github.com>
2022-08-04 02:09:14 -0500

    HomeSection

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -12,7 +12,7 @@ export default function CenteredTabs(props: any) {
   };
 
   return (
-    <Box sx={{ width: '100%', bgcolor: '#333333' }}>
+    <Box sx={{padding:1 ,width: '100%', bgcolor: '#333333' }}>
       <Tabs value={value} onChange={handleChange} textColor="inherit" indicatorColor="secondary" centered>
         <Link href="/" passHref><Tab label="Home" /></Link>
         <Link href="/games" passHref><Tab label="Games" /></Link>
