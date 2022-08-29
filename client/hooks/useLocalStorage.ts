fb7b06bc8cfe074340d2a50475ad1595370e8f3a
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2022-08-29 17:56:16 -0500

    setting up values for login LocalStorage

--- client/hooks/useLocalStorage.ts
+++ client/hooks/useLocalStorage.ts
@@ -1,2 +1,16 @@
 import React from 'react';
-// Testing Something
+
+const setLocalStorageLogin = (val: string) => {
+    if (typeof window !== 'undefined') {
+        localStorage.setItem("login", val);
+    }
+}
+
+const getLocalStorageLogin = () => {
+    let isLoggedIn;
+    if (typeof window !== 'undefined') {
+        isLoggedIn = localStorage.getItem("login");
+    }
+    if (isLoggedIn === null) isLoggedIn = "false"
+    return isLoggedIn;
+}
