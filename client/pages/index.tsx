dbbec4cddefab2b2bba312bd0e403a37fbdc972b
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2022-08-30 15:37:25 -0500

    login page with signup Route

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,20 +1,107 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import Top from '../components/Top'
-import { Button } from '@mui/material'
-import Link from 'next/link'
-import { Route, Routes, Navigate } from "react-router-dom";
-import Signup from "./signup";
-import Login from "./login";
-import Main from "./main";
+import { NextPage } from 'next';
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Game DashBoard
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 const Index: NextPage = () => {
-  return (
-    <div className={styles.container1}>
-     <Link href="/login"><Button variant="outlined" disableElevation color="primary" size='large'>Login</Button></Link>
-     <Link href="/signup"><Button variant="outlined" disableElevation color="primary" size='large'>Sign Up</Button></Link>
-    </div>
-  )
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+return (
+<ThemeProvider theme={theme}>
+  <Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Log In
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          login
+        </Button>
+        <Grid container>
+          <Grid item>
+            <Link href="/signup" variant="body2">
+              {"Don't have an account? Sign Up"}
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+    <Copyright sx={{ mt: 8, mb: 4 }} />
+  </Container>
+</ThemeProvider>
+)
 }
 
 export default Index;
